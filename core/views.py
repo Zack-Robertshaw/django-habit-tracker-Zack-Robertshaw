3f40f10ba185cf33cbbc9972f1c72876a87721a4
Zack Robertshaw <zackrobertshaw@gmail.com>
2021-11-30 18:42:28 -0500

    added ''  path,, added base and home.html templates, registered users, confirmed db's for users and models in Postico

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import User, Habit, Record
+# from .forms import 
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    user = request.user
+    decks = Habit.objects.filter(user=user.pk)
+
+    return render(request, "tracker/home.html", {
+        "user": user,})
+# # "habits": habits,   will be needed eventually.  Don't forget to put it back in.
+#         "user": user, "habits": habits,})
--- project/urls.py
+++ project/urls.py
@@ -16,17 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('', views.home, name='home'),
+
 ]
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns = [
+    urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+    ]
